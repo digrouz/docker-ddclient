7c7ddac56c7096baf57eb110f04b77bd5e52901e
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2017-06-01 14:34:50 +0200

    added autoupgrade feature and improved configureuser function

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,76 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 MYUSER="ddclient"
 MYGID="10007"
 MYUID="10007"
+OS=""
+
+DectectOS(){
+  if [ -e /etc/alpine-release ]; then
+    OS="alpine"
+  elif [ -e /etc/os-release ]; then
+    if /bin/grep -q "NAME=\"Ubuntu\"" /etc/os-release ; then
+      OS="ubuntu"
+    fi
+  fi
+}
+
+AutoUpgrade(){
+  if [ "${OS}" == "alpine" ]; then
+    /sbin/apk --no-cache upgrade
+    /bin/rm -rf /var/cache/apk/*
+  elif [ "${OS}" == "ubuntu" ]; then
+    export DEBIAN_FRONTEND=noninteractive
+    /usr/bin/apt-get update
+    /usr/bin/apt-get -y --no-install-recommends dist-upgrade
+    /usr/bin/apt-get -y autoclean
+    /usr/bin/apt-get -y clean
+    /usr/bin/apt-get -y autoremove
+    /bin/rm -rf /var/lib/apt/lists/*
+  fi
+}
+
+ConfigureUser () {
+  # Managing user
+  if [ -n "${DOCKUID}" ]; then
+    MYUID="${DOCKUID}"
+  fi
+  # Managing group
+  if [ -n "${DOCKGID}" ]; then
+    MYGID="${DOCKGID}"
+  fi
+  local OLDHOME
+  local OLDGID
+  local OLDUID
+  if /bin/grep -q "${MYUSER}" /etc/passwd; then
+    OLDUID=$(/usr/bin/id -u "${MYUSER}")
+    OLDGID=$(/usr/bin/id -g "${MYUSER}")
+    if [ "${DOCKUID}" != "${OLDUID}" ]; then
+      OLDHOME=$(/bin/grep "$MYUSER" /etc/passwd | /usr/bin/awk -F: '{print $6}')
+      /usr/sbin/deluser "${MYUSER}"
+      /usr/bin/logger "Deleted user ${MYUSER}"
+    fi
+    if /bin/grep -q "${MYUSER}" /etc/group; then
+      local OLDGID=$(/usr/bin/id -g "${MYUSER}")
+      if [ "${DOCKGID}" != "${OLDGID}" ]; then
+        /usr/sbin/delgroup "${MYUSER}"
+        /usr/bin/logger "Deleted group ${MYUSER}"
+      fi
+    fi
+  fi
+  if ! /bin/grep -q "${MYUSER}" /etc/group; then
+    /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+  fi
+  if ! /bin/grep -q "${MYUSER}" /etc/passwd; then
+    /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  fi
+  if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
+    /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
+  fi
+  if [ -n "${OLDGID}" ] && [ "${DOCKGID}" != "${OLDGID}" ]; then
+    /usr/bin/find / -group "${OLDGID}" -exec /bin/chgrp ${MYUSER} {} \;
+  fi
+}
 
 ConfigureSsmtp () {
   # Customizing sstmp
@@ -22,46 +90,8 @@ ConfigureSsmtp () {
   fi
 }
 
-ConfigureUser () {
-  # Managing user
-  if [ -n "${DOCKUID}" ]; then
-    MYUID="${DOCKUID}"
-  fi
-  # Managing group
-  if [ -n "${DOCKGID}" ]; then
-    MYGID="${DOCKGID}"
-  fi
-  local OLDHOME
-  local OLDGID
-  local OLDUID
-  bin/grep -q "${MYUSER}" /etc/passwd
-  if [ $? -eq 0 ]; then
-    OLDUID=$(/usr/bin/id -u "${MYUSER}")
-    OLDGID=$(/usr/bin/id -g "${MYUSER}")
-    if [ "${DOCKUID}" != "${OLDUID}" ]; then
-      OLDHOME=$(/bin/echo "~${MYUSER}")
-      /usr/sbin/deluser "${MYUSER}"
-      /usr/bin/logger "Deleted user ${MYUSER}"
-    fi
-    /bin/grep -q "${MYUSER}" /etc/group
-    if [ $? -eq 0 ]; then
-      local OLDGID=$(/usr/bin/id -g "${MYUSER}")
-      if [ "${DOCKGID}" != "${OLDGID}" ]; then
-        /usr/sbin/delgroup "${MYUSER}"
-        /usr/bin/logger "Deleted group ${MYUSER}"
-      fi
-    fi
-  fi
-  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
-  /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
-  if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
-    /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
-  fi
-  if [ -n "${OLDGID}" ] && [ "${DOCKGID}" != "${OLDGID}" ]; then
-    /usr/bin/find / -group "${OLDGID}" -exec /bin/chgrp ${MYUSER} {} \;
-  fi
-}
-
+DectectOS
+AutoUpgrade
 ConfigureUser
 ConfigureSsmtp
 
